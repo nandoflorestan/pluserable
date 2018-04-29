5973919f86989ac71fd8852f6bff54ea7db0d015
Nando Florestan <nandoflorestan@gmail.com>
2018-04-29 10:06:22 +0200

    Assert support for PyPy

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     version='0.5.1.dev1',
     description='Generic user registration for the Pyramid web framework',
     long_description=content_of('README.rst'),
-    classifiers=[  # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=[  # https://pypi.org/pypi?:action=list_classifiers
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
         'License :: OSI Approved :: BSD License',
@@ -59,6 +59,8 @@ setup(
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
