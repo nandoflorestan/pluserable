9df255d173f2f3eb328fae32696d53f8bf68b973
Nando Florestan <nandoflorestan@gmail.com>
2020-03-22 16:18:55 +0100

    Bump version to 0.7.1.dev1 after release

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ requires = [
 
 setup(
     name='pluserable',
-    version='0.7.0',
+    version='0.7.1.dev1',
     description='Generic user registration for the Pyramid web framework',
     long_description=content_of('README.rst'),
     classifiers=[  # https://pypi.org/pypi?:action=list_classifiers
