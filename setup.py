a78129e6b259343370f531fb8eb8468c68b40021
Nando Florestan <nandoflorestan@gmail.com>
2021-02-01 16:45:13 +0100

    Depend on newer bag & kerno

--- setup.py
+++ setup.py
@@ -8,18 +8,17 @@ from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
 
-def content_of(*files, encoding='utf-8'):
+def content_of(*files, encoding="utf-8"):
     """Return the content of ``files`` (which should be paths)."""
     here = os.path.abspath(os.path.dirname(__file__))
     content = []
     for f in files:
         with open(os.path.join(here, f), encoding=encoding) as stream:
             content.append(stream.read())
-    return '\n'.join(content)
+    return "\n".join(content)
 
 
 class PyTest(TestCommand):
-
     def finalize_options(self):
         TestCommand.finalize_options(self)
         # self.test_args = []
@@ -28,54 +27,54 @@ class PyTest(TestCommand):
     def run_tests(self):
         # Import here, because outside requirements aren't installed
         import pytest
+
         result = pytest.main(self.test_args)
         sys.exit(result)
 
 
 requires = [
-    'bag >= 3.0.0.dev1',
-    'kerno >= 0.5.0.dev1',
-    'sqlalchemy',
-    'transaction',
-    'cryptacular',
-    'deform',
-    'pyramid',          # TODO REMOVE when agnostic
-    'pyramid_mailer',
-    'pyramid_mako',
-    'zope.interface',
+    "bag >= 3.1.0",
+    "kerno >= 0.5.0",
+    "sqlalchemy",
+    "transaction",
+    "cryptacular",
+    "deform",
+    "pyramid",  # TODO REMOVE when agnostic
+    "pyramid_mailer",
+    "pyramid_mako",
+    "zope.interface",
 ]
 
 setup(
-    name='pluserable',
-    version='0.7.1.dev1',
-    description='Generic user registration for the Pyramid web framework',
-    long_description=content_of('README.rst'),
+    name="pluserable",
+    version="0.7.1.dev1",
+    description="Generic user registration for the Pyramid web framework",
+    long_description=content_of("README.rst"),
     classifiers=[  # https://pypi.org/pypi?:action=list_classifiers
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        'License :: OSI Approved :: BSD License',
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Libraries :: Python Modules",
     ],
-    author='Nando Florestan',
-    author_email='nandoflorestan@gmail.com',
-    url='https://github.com/nandoflorestan/pluserable',
-    keywords=[
-        'authentication', 'horus', 'pyramid', 'user', 'registration'],
-    license='BSD',
+    author="Nando Florestan",
+    author_email="nandoflorestan@gmail.com",
+    url="https://github.com/nandoflorestan/pluserable",
+    keywords=["authentication", "horus", "pyramid", "user", "registration"],
+    license="BSD",
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     install_requires=requires,
-    tests_require=requires + ['pytest', 'mock', 'webtest'],
-    cmdclass={'test': PyTest},
-    test_suite='tests',
+    tests_require=requires + ["pytest", "mock", "webtest"],
+    cmdclass={"test": PyTest},
+    test_suite="tests",
 )
