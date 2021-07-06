04910526515f617fb9376e69fb22503374f6fb2f
Nando Florestan <nandoflorestan@gmail.com>
2021-07-06 17:40:43 +0200

    Fix tests

--- tests/integration/test_init.py
+++ tests/integration/test_init.py
@@ -1,23 +1,20 @@
 """Test groupfinder and request.user."""
 
 from datetime import datetime
-from unittest.mock import Mock
 
 from pluserable.web.pyramid import get_user
 
-from tests.integration import IntegrationTestBase
+from tests.integration import LoggedIntegrationTest
 
 
-class TestInitCase(IntegrationTestBase):  # noqa
+class TestInitCase(LoggedIntegrationTest):  # noqa
 
     def test_get_user_fetches_existing_user(self):
         """Fake an authenticated user and see if it appears as request.user."""
-        the_user = self.create_users(count=1)
-        self.repo.flush()
-        request = Mock()
-        request.repo = self.get_request().repo
-        request.unauthenticated_userid = 1
+        request = self.get_request()
+        assert request.unauthenticated_userid == 1
+        assert request.identity
         user = get_user(request)
-        assert user is the_user
+        assert user is request.identity
         assert user.registered_date == datetime(2000, 1, 1)
         assert user.last_login_date == datetime(2000, 1, 1)
